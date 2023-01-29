72ca4a6e0605e4425a540ae3871dd47e4d344405
Rohan Kulkarni <rohank2502@gmail.com>
2023-01-29 22:21:17 +0530

    web version 1

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+'yellow-sea': {
+  DEFAULT: '#FFB000',
+  50: '#FFE9B8',
+  100: '#FFE3A3',
+  200: '#FFD67A',
+  300: '#FFC952',
+  400: '#FFBD29',
+  500: '#FFB000',
+  600: '#C78900',
+  700: '#8F6300',
+  800: '#573C00',
+  900: '#1F1500'
+},
+    }
+    },
+      fontFamily: {
+
+    batangas: ["Batangas_Bold_700"],
+  },
+  },
+  plugins: [],
+}
